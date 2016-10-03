374e38756e8ea434930f8a55416801a6fb244237
chtmorris <agoosester@gmail.com>
2016-10-03 17:10:28 +0800

    upgraded to swift 3

--- GAFilterPhotoApp/ViewController.swift
+++ GAFilterPhotoApp/ViewController.swift
@@ -23,7 +23,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func applyFilterButtonPressed(sender: UIBarButtonItem) {
+    @IBAction func applyFilterButtonPressed(_ sender: UIBarButtonItem) {
     
         // Create an image to filter
         let inputImage = CIImage(image: dogImage.image!)
@@ -32,13 +32,13 @@ class ViewController: UIViewController {
         let randomColor = [kCIInputAngleKey: (Double(arc4random_uniform(314)) / 100)]
         
         // Apply a filter to the image
-        let filteredImage = inputImage!.imageByApplyingFilter("CIHueAdjust", withInputParameters: randomColor)
+        let filteredImage = inputImage!.applyingFilter("CIHueAdjust", withInputParameters: randomColor)
         
         // Render the filtered image
-        let renderedImage = context.createCGImage(filteredImage, fromRect: filteredImage.extent)
+        let renderedImage = context.createCGImage(filteredImage, from: filteredImage.extent)
         
         // Reflect the change back in the interface
-        dogImage.image = UIImage(CGImage: renderedImage)
+        dogImage.image = UIImage(cgImage: renderedImage!)
     
     }
     
